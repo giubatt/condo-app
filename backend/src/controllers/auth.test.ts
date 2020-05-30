b1b79dc61d473b93c7fc302685bb005e52070617
Giuseppe Battistella <giusbattistella@gmail.com>
2020-05-30 16:54:40 -0300

    Added auth controller

---
+++ backend/src/controllers/auth.test.ts
@@ -0,0 +1,82 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import faker from 'faker'
+import * as AuthController from './auth'
+import { User } from '../models/user'
+
+const mongod = new MongoMemoryServer()
+
+beforeAll(async (done) => {
+  await mongoose.connect(await mongod.getUri(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  done()
+})
+
+afterEach(async (done) => {
+  await mongoose.connection.dropDatabase()
+  done()
+})
+
+afterAll(async (done) => {
+  await mongoose.disconnect()
+  await mongod.stop()
+  done()
+})
+
+describe(`login`, () => {
+  test(`throws error on inexistent user`, async () => {
+    // Act
+    const actual = AuthController.login({
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    })
+
+    // Assert
+    await expect(actual).rejects.toThrowErrorMatchingInlineSnapshot(`"InvalidCredentials"`)
+  })
+
+  test(`throws error on incorrect credentials`, async () => {
+    // Arrange
+    const data = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+    await User.create(data)
+
+    // Act
+    const actual = AuthController.login({
+      ...data,
+      password: `test`,
+    })
+
+    // Assert
+    await expect(actual).rejects.toThrowErrorMatchingInlineSnapshot(`"InvalidCredentials"`)
+  })
+
+  test(`returns token on correct credentials`, async () => {
+    // Arrange
+    const data = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+    await User.create(data)
+
+    // Act
+    const actual = await AuthController.login(data)
+
+    // Assert
+    expect(actual).toEqual(expect.any(String))
+  })
+})
+
+describe(`decodeToken`, () => {
+  test(`throws error on invalid token`, async () => {
+    // Act
+    // Assert
+    expect(() => AuthController.decodeToken(`invalid_token`)).toThrowErrorMatchingInlineSnapshot(`"InvalidToken"`)
+  })
+})
---
+++ backend/src/controllers/auth.ts
@@ -0,0 +1,28 @@
+import { User, UserDocument } from '../models/user'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../utils/secrets'
+import { Types } from 'mongoose'
+
+export const login = async ({ email, password }: { email: string; password: string }): Promise<string> => {
+  const user = await User.findOne({ email })
+  if (!user) throw new Error(`InvalidCredentials`)
+
+  const validPassword = await user.comparePassword(password)
+  if (!validPassword) throw new Error(`InvalidCredentials`)
+
+  return jwt.sign(
+    {
+      id: user.id,
+    },
+    JWT_SECRET,
+  )
+}
+
+export const decodeToken = (token: string): { id: Types.ObjectId } => {
+  try {
+    const payload = jwt.verify(token, JWT_SECRET) as { id: Types.ObjectId }
+    return payload
+  } catch (error) {
+    throw new Error(`InvalidToken`)
+  }
+}
