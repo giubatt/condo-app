c034fbcaed57882b70916fe19b81fd84c9ff57e1
Giuseppe Battistella <giusbattistella@gmail.com>
2020-05-29 22:32:36 -0300

    Added server

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import { graphqlServer } from './graphql/server'
+import mongoose from 'mongoose'
+import { MONGODB_URI } from './utils/secrets'
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+const app = express()
+// graphqlServer.applyMiddleware({ app })
+
+export default app
---
+++ backend/src/server.ts
@@ -0,0 +1,6 @@
+import app from './app'
+import { PORT } from './utils/secrets'
+
+app.listen({ port: process.env.PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:${PORT}`),
+)
