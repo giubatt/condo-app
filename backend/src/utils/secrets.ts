0d9ea86949c8d4bc868767c0d2a54ede711e92f4
Giuseppe Battistella <giusbattistella@gmail.com>
2020-06-03 22:06:55 -0300

    Moved env inside each project

--- backend/src/utils/secrets.ts
+++ backend/src/utils/secrets.ts
@@ -1,20 +1,19 @@
 import logger from './logger'
 import dotenv from 'dotenv'
-import path from 'path'
 
-dotenv.config({ path: path.resolve(process.cwd(), `../`, `.env`) })
+dotenv.config()
 
 export const ENVIRONMENT = process.env.NODE_ENV
-export const PORT = process.env[`BACKEND_PORT`] || 3000
-export const JWT_SECRET = process.env[`BACKEND_JWT_SECRET`]
-export const MONGODB_URI = process.env[`BACKEND_MONGODB_URI`]
+export const PORT = process.env[`PORT`] || 3000
+export const JWT_SECRET = process.env[`JWT_SECRET`]
+export const MONGODB_URI = process.env[`MONGODB_URI`]
 
 if (!JWT_SECRET) {
-  logger.error(`No jwt secret. Set BACKEND_JWT_SECRET environment variable.`)
+  logger.error(`No jwt secret. Set JWT_SECRET environment variable.`)
   process.exit(1)
 }
 
 if (!MONGODB_URI) {
-  logger.error(`No mongo connection string. Set BACKEND_MONGODB_URI environment variable.`)
+  logger.error(`No mongo connection string. Set MONGODB_URI environment variable.`)
   process.exit(1)
 }
