19c1ade06ed6345a26333f8291c379b9b7cfaeaf
Giuseppe Battistella <giusbattistella@gmail.com>
2020-06-04 23:30:26 -0300

    Added variants to button

--- frontend/src/components/elements/Button.tsx
+++ frontend/src/components/elements/Button.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 
-const Button: React.FC<React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->> = ({ children, className, ...props }) => {
+export interface Props
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  variant?: 'simple' | 'outline'
+}
+
+const Button: React.FC<Props> = ({
+  children,
+  className,
+  variant = 'simple',
+  ...props
+}) => {
+  const buttonClasses = {
+    simple:
+      'bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline',
+    outline:
+      'bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded',
+  }[variant]
+
   return (
-    <button
-      className={
-        'bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ' +
-        className
-      }
-      {...props}
-    >
+    <button className={`${buttonClasses} ${className}`} {...props}>
       {children}
     </button>
   )
