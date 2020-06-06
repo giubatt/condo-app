5bde84d5c8c516f69c73a87d9a20e5b2278b2404
Giuseppe Battistella <giusbattistella@gmail.com>
2020-06-05 21:09:33 -0300

    Added Date graphql scalar

--- backend/src/graphql/schemas/index.ts
+++ backend/src/graphql/schemas/index.ts
@@ -1,15 +1,24 @@
 import { makeExecutableSchema, gql } from 'apollo-server-express'
 import { resolvers as authResolvers } from './auth/resolvers'
 import { resolvers as apartmentResolvers } from './apartment/resolvers'
+import { resolvers as tenantResolvers } from './tenant/resolvers'
+
 import { typeDefs as authTypeDefs } from './auth/typeDefs'
 import { typeDefs as apartmentTypeDefs } from './apartment/typeDefs'
+import { typeDefs as tenantTypeDefs } from './tenant/typeDefs'
+
+import { DateTimeResolver, DateTimeTypeDefinition } from 'graphql-scalars'
 
 const typeDefs = gql`
   type Query
   type Mutation
 `
 
+const scalarsResolvers = {
+  DateTime: DateTimeResolver,
+}
+
 export const schema = makeExecutableSchema({
-  typeDefs: [typeDefs, authTypeDefs, apartmentTypeDefs],
-  resolvers: [authResolvers, apartmentResolvers],
+  typeDefs: [typeDefs, authTypeDefs, apartmentTypeDefs, tenantTypeDefs, DateTimeTypeDefinition],
+  resolvers: [authResolvers, apartmentResolvers, tenantResolvers, scalarsResolvers],
 })
