38fee2af1976dfe2aa7473febb7b1f748e806108
Giuseppe Battistella <giusbattistella@gmail.com>
2020-06-06 12:29:51 -0300

    Updated styles

--- frontend/src/components/apartment/List.tsx
+++ frontend/src/components/apartment/List.tsx
@@ -9,6 +9,7 @@ export type Apartment = {
 
 export interface Props {
   items?: Apartment[]
+  selectedId?: string
   onDelete: (id: string) => void
   onEdit: (arg0: Apartment) => void
   onOpenApartment: (id: string) => void
@@ -16,6 +17,7 @@ export interface Props {
 
 const List: React.FC<Props> = ({
   items,
+  selectedId,
   onDelete,
   onEdit,
   onOpenApartment,
@@ -25,47 +27,49 @@ const List: React.FC<Props> = ({
       <table className="min-w-full">
         <thead>
           <tr>
-            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-3 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
               Apartamento
             </th>
-            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-3 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
               Bloco
             </th>
-            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+            <th className="px-3 py-3 border-b border-gray-200 bg-gray-50"></th>
           </tr>
         </thead>
         <tbody className="bg-white">
           {items?.map(({ id, number, block }) => (
             <tr
               key={id}
-              className="hover:bg-gray-100 cursor-pointer"
+              className={`cursor-pointer ${
+                selectedId === id ? 'bg-gray-100' : 'hover:bg-gray-100'
+              }`}
               onClick={() => onOpenApartment(id)}
             >
-              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <td className="px-3 py-4 whitespace-no-wrap border-b border-gray-200">
                 <span className="text-sm leading-5 text-gray-800">
                   {number}
                 </span>
               </td>
-              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <td className="px-3 py-4 whitespace-no-wrap border-b border-gray-200">
                 <span className="text-sm leading-5 text-gray-800">{block}</span>
               </td>
-              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 flex">
-                <a href="#" className="text-gray-600 hover:text-gray-900">
+              <td className="px-3 py-4 whitespace-no-wrap border-b border-gray-200 flex justify-end">
+                <button className="text-gray-600 hover:text-gray-900">
                   <Edit
                     onClick={(e) => {
                       e.stopPropagation()
                       onEdit({ id, number, block })
                     }}
                   />
-                </a>
-                <a href="#" className="text-gray-600 hover:text-gray-900 pl-2">
+                </button>
+                <button className="text-gray-600 hover:text-gray-900 pl-2">
                   <Trash2
                     onClick={(e) => {
                       e.stopPropagation()
                       onDelete(id)
                     }}
                   />
-                </a>
+                </button>
               </td>
             </tr>
           ))}
